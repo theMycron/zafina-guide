ff2b658339bd712bf984d5dc9363b70f71de1935
theMycron <alyasfy12@gmail.com>
2025-04-03 10:23:39 +0300

    feat: add MUI with nextjs support and navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,11 @@
 import type { Metadata } from 'next';
+import { NavBar } from 'components/NavBar'
+import Box from '@mui/material/Box';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 export const metadata: Metadata = {
   title: 'Next.js on GitHub Pages',
@@ -12,7 +19,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+				<AppRouterCacheProvider>
+					<NavBar />
+					<Box component="main" sx={{ p: 3 }}>
+						{children}
+					</Box>
+				</AppRouterCacheProvider>
+			</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 export default function Home() {
   return (
-    <main>
-      <div>Next.js on GitHub Pages</div>
-    </main>
+		<div>Zafina Guide</div>
   );
 }
